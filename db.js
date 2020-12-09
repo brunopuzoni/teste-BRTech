17478b1b2b8c7e8bb65116b11a903f566315a9ec
Bruno Puzoni <69810132+brunopuzoni@users.noreply.github.com>
2020-12-09 01:14:02 -0300

    Update db.js
    Added a few test rows into the database

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./todo.db')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./todo.db');
 
-db.serialize(function() {
-    // criar a tabela
-    db.run(`
+db.serialize(function () {
+    const banco = `
         CREATE TABLE IF NOT EXISTS tarefas(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             descricao TEXT,
             status INTEGER
         );
-    `)
-})
+    `;
 
-module.exports = db
+    db.run(banco);
+    db.run('INSERT INTO tarefas(descricao, status) VALUES(?,?)',['Tarefa 1',1]);
+    db.run('INSERT INTO tarefas(descricao, status) VALUES(?,?)',['Tarefa 2',0]);
+    db.run('INSERT INTO tarefas(descricao, status) VALUES(?,?)',['Tarefa 3',1]);
+    db.run('INSERT INTO tarefas(descricao, status) VALUES(?,?)',['Tarefa 4',0]);
+});
+
+module.exports = db;
