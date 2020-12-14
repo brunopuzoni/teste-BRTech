4a682d219313b2f38dfe81deef83cc96caf5328a
Bruno Puzoni <69810132+brunopuzoni@users.noreply.github.com>
2020-12-14 17:56:00 -0300

    Update scripts.js
    Removed XMLHttpRequest function, added another bootstrap modal function with jQuery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,42 +1,21 @@
-const form = document.getElementById('form-update')
-const formJson = JSON.stringify($('form-update').serializeArray());
-console.log(formJson);
-
-function updateForm() {
-
-  // let descr = 'updtarefa=' + document.getElementById('updtarefa').value;
-  // let fid = 'updid=' + document.getElementById('updid').value;
-  let id = document.getElementById('updid').value;
-
-  var url = 'http://localhost:3001/update/' + id + '';
-  console.log(descr);
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("PATCH", url);
-
-  xhr.setRequestHeader("Accept", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("Accept-Patch", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-    }
-  };
-
-  xhr.send(formJson);
-
-}
-
 $('#modal-update').on('show.bs.modal', function (event) {
   let btn = $(event.relatedTarget)
   let id = btn.data('id')
   let desc = btn.data('descricao')
-
   let modal = $(this)
+
   modal.find('#updtarefa').val(desc)
   modal.find('#updid').val(id)
-
   modal.find('#form-update').attr('action', '/update/' + id + '')
+});
+
+$('#modal-delete').on('show.bs.modal', function (event) {
+  let btn = $(event.relatedTarget)
+  let id = btn.data('id')
+  let desc = btn.data('descricao')
+  let modal = $(this)
+
+  modal.find('#delid').val(id)
+  modal.find('#delete-text').text('Deseja realmente excluir a tarefa "' + desc + '"?')
+  modal.find('#form-delete').attr('action', '/delete/' + id + '')
 })
